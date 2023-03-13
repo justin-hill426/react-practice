215c6cd80f402e956afa6b10eab717e60dc6ea98
justin-hill426 <jrhill426@gmail.com>
2023-03-12 23:01:11 -0600

    Added a star display component

--- star-game/src/StarMatch.js
+++ star-game/src/StarMatch.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import { useState } from 'react';
 
+const StarsDisplay = props => {
+  <>
+    {
+      utils.range(1, props.count).map(starId => 
+        <div key={starId} className='star' />  
+    )}
+  </>
+}
+
 const PlayNumber = props => {
   <button className='number' onClick={() => console.log("Num", props.number)}>
     {props.number}
@@ -16,10 +25,7 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-          {
-            utils.range(1, stars).map(starId => 
-            <div key={starId} className='star' />  
-          )}
+          <StarsDisplay count={stars}/>
         </div>
         <div className="right">
          {
